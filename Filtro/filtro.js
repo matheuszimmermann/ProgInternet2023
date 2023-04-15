b12da4bafa87463518467e87d66f754e4b52e179
Matheus Zimmermann <125280019+matheuszimmermann@users.noreply.github.com>
2023-04-14 22:12:06 -0300

    Update filtro.js

--- Filtro/filtro.js
+++ Filtro/filtro.js
@@ -5,43 +5,20 @@ const listEl = document.getElementById("list");
 const searchField = document.getElementById("searchField");
 searchField.addEventListener("input", inputHandler)
 
-
-
 this.fillList();
 
-
-
 function adicionar() {
- 
     var searchField = document.getElementById("searchField");
     var nome = searchField.value; 
     nameList.push(nome);
 
-    for(var i = 0; i < nameList.length; i++ ){
-        lista += (i + 1) + "." + nameList[i] + "\n";
-    }
-    
+    fillList();
+
     searchField.value = "";
     searchField.focus();
 }
 var btAdicionar = document.getElementById("btAdicionar");
 btAdicionar.addEventListener("click", adicionar);
-
-
-function remover(){
- 
-    var searchField = document.getElementById("searchField");
-    var nameList = searchField.value; 
-
-    for(var i = 0; i < nameList.length; i++ ){
-        lista = (i - nameList) + nameList[i];
-    }
-    
-    searchField.value = "";
-}
-var btRemover = document.getElementById("btRemover");
-btRemover.addEventListener("click", remover);
-
 
 
 function fillList(list = nameList) {
@@ -52,7 +29,6 @@ function fillList(list = nameList) {
        listEl.appendChild(listItems);
    }
 }
-
 
 
 function inputHandler() {
